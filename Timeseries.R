94fcb370f004f7367d07322c25097a0b1f4c4e94
Steven <info@truediggs.com>
2016-11-24 16:48:55 -0500

    add sp monthly

--- Timeseries.R
+++ Timeseries.R
@@ -41,9 +41,10 @@ for (i in 1:nrow(gold)) {
 
 #create training and test data for to gague forecast accuracy 
 gold_train <- subset(gold, Date > as.Date("2016-01-01") )
-gold_test <- subset(gold, Date < as.Date("2016-01-01"))
+gold_test <- subset(gold, Date < as.Date("2016-01-01")))
 
 
+length(gold$Date > as.Date("2016-01-01"))
 #read in oil 
 oil <- read.csv("oil2016.csv", stringsAsFactors=FALSE, header=TRUE)
 oil$Date <- as.Date(oil$Date, "%d-%B-%y")
@@ -57,9 +58,19 @@ for (i in 1:nrow(oil)) {
   oil$avg_price[i] <- (sum(submonth$Price))/length(submonth$Price)
 }
 
+#s&p500 monthly 
+sp['day_year'] <- strftime(sp$Date, "%m/%y")
+sp['avg_price'] <- 0
+for (i in 1:nrow(sp)) {
+  submonth <- subset(sp, day_year == day_year[i])
+  sp$avg_price[i] <- (sum(submonth$Price))/length(submonth$Price)
+}
+
 #create test/train data for oil 
 oil_train <- subset(oil, Date > as.Date("2016-01-01") )
 oil_test <- subset(oil, Date < as.Date("2016-01-01"))
+
+
 #merge
 
 oil['oil'] <- oil$Price 
@@ -75,16 +86,26 @@ timeseries <- subset(timeseries, select=c("Date", "oil", "sp", "gold", "gold_avg
 timeseries_month <- unique(timeseries$gold_avg, incomparables=FALSE)
 #same for oil 
 timeseries_month_oil <- unique(oil$avg_price, incomparables=FALSE)
+#same for S&P
+timeseries_month_sp <- unique(sp$avg_price, incomparables=FALSE)
 
 #create time series
 timeseries_all <- subset(timeseries, select=c("oil", "sp", "gold"))
 ts.all <- ts(data=timeseries_all)
+
 ts.oil<-ts(timeseries_all$oil, start=c(2010))
 ts.oil_avg <- ts(timeseries_month_oil, start=c(2010), frequency=12)
 
-ts.gold <- ts(timeseries_all$gold)
+ts.gold <- ts(timeseries_all$gold, start=c(2010), frequency=5)
+ts.gold
+plot.ts(ts.gold)
 ts.gold_avg <- ts(timeseries_month, start=c(2010), frequency=12)
+
 ts.sp <- ts(timeseries_all$sp)
+ts.sp_avg <- ts(timeseries_month_sp,  start=c(2010), frequency=12 )
+
+
+
 
 #combine oil and gold monthly 
 ts.combo <- cbind(ts.oil_avg, ts.gold_avg)
@@ -109,15 +130,42 @@ plot(ts.oil)
 ts.gold.1.d <- decompose(ts.gold.1)
 plot(ts.gold.1.d)
 
-#begin forecast 
-library(forecast)
-gold.holt <- HoltWinters(ts.gold_avg, gamma=FALSE)
-gold.holt
-plot(gold.holt)
+ts.sp_avg.d <- decompose(ts.sp_avg)
+plot(ts.sp_avg.d)
 
+#begin forecast with HoltWinters 
+library(forecast)
+#----gold monthly 
+gold.holt <- HoltWinters(ts.gold_avg, gamma=FALSE)
+plot(gold.holt)
+gold.holt_fore <- forecast.HoltWinters(gold.holt, h=12)
+
+plot(gold.holt_fore)
+
+#----gold daily 
+gold.daily.holt <- HoltWinters(ts_goldgold, gamma=FALSE)
+gold.daily.holt.fore <- forecast.HoltWinters(gold.daily.holt)
+plot(gold.daily.holt.fore)
+
+
+#----oil monthly 
 oil.holt <- HoltWinters(ts.oil_avg, gamma=FALSE)
-oil.holt
 plot(oil.holt)
+oil.holt_fore <- forecast.HoltWinters(oil.holt, h=12)
+
+plot(oil.holt_fore)
+
+#try seasonal forecast 
+#---gold seasonal 
+gold.holt.seasonal <- HoltWinters(ts.gold_avg, gamma=TRUE)
+plot(gold.holt.seasonal)
+gold.holt.seasonal <- forecast.HoltWinters(gold.holt.seasonal, h=12)
+plot(gold.holt.seasonal)
+
+#oil seasonal 
+oil.holt.seasonal <- HoltWinters(ts.gold_avg, gamma=TRUE)
+oil.holt.seasonal.fore <- forecast.HoltWinters(oil.holt.seasonal, gamma=TRUE)
+plot(oil.holt.seasonal.fore)
 
 
 #check if data is stationary 
@@ -135,12 +183,22 @@ fit_1<- arima(ts.gold_avg, c(0,0,0))
 par(mfrow=c(1,1))
 tsdiag(fit1)
 
+fit_2<- arima(ts.oil_avg, c(0,0,0))
+tsdiag(fit2)
+
 #get lazy and have R create fit for you 
 fit_gold<- auto.arima(ts.gold_avg)
-tsdiag(fit2)
 fit_gold
+tsdiag(fit_gold)
+fit_gold <- arima(ts.gold_avg, c(1, 1, 1))
+
+fit_oil <- auto.arima(ts.oil_avg)
+fit_oil
+tsdiag(fit_oil)
+
+fit_sp <- auto.arima()
 #create forecast 
 
-fit_gold <- forecast.Arima(fit_gold)
+fit_gold <- forecast.Arima(fit_gold, h=12)
 plot(fit_gold)
 summary(fit_gold)
